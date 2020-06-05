153b604475f5f52a896d817b46ab04c7cdafbb4c
Sandor Desi <d.alex.galaxy@gmail.com>
2020-06-05 18:16:22 +0100

    Missing Images- Windows workstation

--- App.js
+++ App.js
@@ -2,7 +2,9 @@ import React from "react";
 import { View } from "react-native";
 
 import ViewImageScreen from "./app/screens/ViewImageScreen";
+import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
+
 
 export default function App() {
-  return <ViewImageScreen />;
+  return <ListingDetailsScreen />;
 }
