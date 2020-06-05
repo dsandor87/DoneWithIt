1a1100c05d5b2f8a755b2e0e0537d590b2b1aac8
Sandor Desi <d.alex.galaxy@gmail.com>
2020-06-05 18:01:45 +0100

    Add More Screens,Windows workstation

--- App.js
+++ App.js
@@ -1,17 +1,8 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import WelcomeScreen from './app/screens/WelcomeScreen'
-import ViewImageScreen from './app/screens/ViewImageScreen'
+import React from "react";
+import { View } from "react-native";
+
+import ViewImageScreen from "./app/screens/ViewImageScreen";
 
 export default function App() {
-  return <ViewImageScreen />
+  return <ViewImageScreen />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-})
